43749efb80f9acd29dbc2604cb7861ca188726c6
Cyril Galibern <cyril.ggalibern@free.fr>
2015-06-05 01:38:41 +0200

    can post and delete a service

--- routes/svcapp.js
+++ routes/svcapp.js
@@ -1,19 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+var bodyParser = require('body-parser');
+var urlencode = bodyParser.urlencoded({ extended: false });
+
+var services = [ "svc0", "svc1"]
+
 router.route('')
   .get(function(request, response){
-     response.json('svcone');
-  });
+     response.json(services);
+  })
 
-router.route('/:id')
-  .get(function(request, response){
-     response.json("service id "+ request.params.id);
+  .post(urlencode, function(request, response) {
+    var service = request.body;
+    services.push(service.svcname);
+    response.status(201).json("created new service: "+service.svcname);
   });
 
 router.route('/about')
-  .get(function(request, response) {
-    response.send('About svcmt');
-  });
+    .get(function(request, response) {
+      response.send('About svcmgt');
+    });
+
+router.route('/:id')
+  .get(function(request, response){
+     var id = request.params.id;
+     if (!services[id]) { response.status(404).json('not found');}
+     else { response.json("service id "+ id + ': ' + services[id]);}
+  })
+
+  .delete(function(request, response) {
+    var id = request.params.id;
+    if (services[id]=="") { response.status(404).json('not found');}
+    services.splice(id, 1);
+    response.status(204).json("delete service id: "+id);
+    });
+
+
 
 module.exports = router;
--- test.js
+++ test.js
@@ -12,10 +12,16 @@ describe('Requests to the /svcmgt path', function() {
       .get('/svcmgt')
       .expect('Content-Type', /json/, done);
   });
-  it('Returns content with svcone', function(done) {
-    request(app)
-      .get('/svcmgt')
-      .expect(/svcone/i, done);
+  it('Returns content with svc0, and svc1', function(done) {
+    request(app).get('/svcmgt').expect(/svc0/i).expect(/svc1/i, done);
+    });
+  it('Can post new service', function(done) {
+      request(app).post('/svcmgt').send('svcname=svc2').expect(201, done);
+    });
+  it('Can post new service, then retreive it', function(done) {
+    request(app).post('/svcmgt').send('svcname=svc3').expect(201, function(){
+      request(app).get('/svcmgt').expect(/svc3/i,done);
+    });
   });
 });
 
@@ -33,14 +39,15 @@ describe('Requests to the /svcmgt/about path', function() {
 });
 
 describe('Requests to the /svcmgt/:id path', function() {
-  it('Returns a 200 status code', function(done) {
-    request(app)
-      .get('/svcmgt/13')
-      .expect(200, done);
+  it('Returns a 200 status code on existing service 3', function(done) {
+    request(app).get('/svcmgt/3').expect(200, done);
   });
-  it('Returns correct service id content', function(done) {
-    request(app)
-      .get('/svcmgt/12')
-      .expect(/service id 12/i, done);
+  it('Returns correct service 3 name content', function(done) {
+    request(app).get('/svcmgt/3').expect(/svc3/i, done);
+  });
+  it('service id 3: Delete must 204', function(done) {
+    request(app).delete('/svcmgt/3').expect(204, function() {
+      request(app).get('/svcmgt/3').expect(404, done);
+    });
   });
 });
